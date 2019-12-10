06519ff419794b423ff3494c036db7b41e351e4c
Yuepei <liyp0095@gmail.com>
2019-12-10 01:55:09 -0600

    add random search with keras and scklearn

---
+++ Random Search/dnn_model.py
@@ -0,0 +1,25 @@
+#!/bin/python
+# -*- coding: utf8 -*-
+
+'''
+Description: generate dnn model for random search
+Date: 2019-12-10
+'''
+
+class DNN_Model:
+    def __init__(hpara, data):
+        self.x_train = data.x_train
+        self.y_train = data.y_train
+        self.x_test = data.x_test
+        self.y_test = data.y_test
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
