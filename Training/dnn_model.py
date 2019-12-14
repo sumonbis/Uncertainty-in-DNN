6658bb3bc9c2efe6d711fed3207484d73a9d0d19
Sumon Biswas <sumon@iastate.edu>
2019-12-14 15:22:08 -0600

    Verbose is off, evaluate returns only the acc

--- Training/dnn_model.py
+++ Training/dnn_model.py
@@ -1,11 +1,6 @@
 #!/bin/python
 # -*- coding: utf8 -*-
 
-'''
-Description: generate dnn model for random search
-Date: 2019-12-10
-'''
-
 from __future__ import print_function
 import keras
 from keras.datasets import mnist
@@ -14,6 +9,7 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 batch_size = 128
@@ -27,36 +23,14 @@ img_rows, img_cols = 28, 28
 def input_data(percent):
     # the data, split between train and test sets
     dataset = mnist.load_data()
-
     (x_train, y_train), (x_test, y_test) = dataset
-
-    #print(x_train.shape[0])
-
-    #x_train = np.random.shuffle(x_train)
-
-
-
-
-    # y_train = np.random.shuffle(y_train)
-
     indices = np.arange(x_train.shape[0])
     np.random.shuffle(indices)
-    #
     x_train = x_train[indices]
-    # print(x_train[indices])
-    #
-    # indices = np.arange(y_train.shape[0])
-    # np.random.shuffle(indices)
     y_train = y_train[indices]
 
     x_train = x_train[:int(x_train.shape[0] * percent / 100)]
     y_train = y_train[:int(y_train.shape[0] * percent / 100)]
-
-    # select a part of the dataset for training
-    # x = np.concatenate((x_train, x_test))
-    # y = np.concatenate((y_train, y_test))
-    # train_size = 0.1
-    # x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_size) #, random_seed=2019
 
     if K.image_data_format() == 'channels_first':
         x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -108,10 +82,11 @@ def create_model(hidden_unit):
 
 
 def train(model, x_train, y_train, x_test, y_test, epoch):
+    print("\nTraining ...")
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
-              verbose=1,
+              verbose=0,
               validation_data=(x_test, y_test))
     return model
 
@@ -119,18 +94,17 @@ def evaluate(model, x_test, y_test):
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
-    return score[0], score[1]
+    #return score[0], score[1]
+    return score[1]
 
 def train_and_evaluate(percent, epoch, hidden_unit):
     x_train, y_train, x_test, y_test, input_shape = input_data(percent)
     dnn_model = create_model(hidden_unit)
     model = train(dnn_model, x_train, y_train, x_test, y_test, epoch)
     return evaluate(model, x_test, y_test)
-    pass
 
 def main():
-    loss, acc = train_and_evaluate(100, 1, 64)
-    print(loss)
+    acc = train_and_evaluate(10, 1, 64)
     print(acc)
 
 if __name__ == "__main__":
