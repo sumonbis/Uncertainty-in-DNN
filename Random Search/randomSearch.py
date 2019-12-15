166134351743f8fb949a1e0d09ed0112c6694e76
liyp0095 <liyp0095@gmail.com>
2019-12-14 23:56:40 -0600

    random search test result

--- Random Search/randomSearch.py
+++ Random Search/randomSearch.py
@@ -16,41 +16,56 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import RandomizedSearchCV
 
 
-def randomSearch(distributions, x_train, y_train, x_test, y_test, n_iter):
-    # epoch = 2
+def randomSearch(distributions, x_train, y_train, x_test, y_test, no_of_trials, index_exp):
+    # epoch = 1
+    file = open("result/rs_experiment_" + str(index_exp+1)+"_detail.csv", "w", encoding="utf-8")
+    file.write("hidden unit size, accuracy\n")
+
     para = []
     acc = []
-    for i in range(n_iter):
+    for i in range(no_of_trials):
         hidden_unit = random.choice(distributions)
+        print(" + random search {} iteration : hidden_unit = {}".format(i, hidden_unit))
         model = create_model(hidden_unit);
-        model = train(model, x_train, y_train, x_test, y_test, 2)
-        loss, accuracy = evaluate(model, x_test, y_test)
+        model = train(model, x_train, y_train, x_test, y_test, 1)
+        accuracy = evaluate(model, x_test, y_test)
+        print("   accuracy = {}".format(accuracy))
+        file.write(str(hidden_unit)+','+str(accuracy)+"\n")
         para.append(hidden_unit)
         acc.append(accuracy)
+    file.close()
     accuracy = max(acc)
     parameter = para[acc.index(accuracy)]
     return parameter
 
 
 def main():
-    x_train, y_train, x_test, y_test, input_shape = input_data(30)
+    file_rs = open("result/randomSearch.csv", "w", encoding="utf-8")
+    file_rs.write("hidden unit size, accuracy\n")
+
+    no_of_exp = 10
+    no_of_trials = 10
+
+    # x_train, y_train, x_test, y_test, input_shape = input_data(30)
+    x_train, y_train, x_test, y_test, input_shape = input_data(30,100)
+    distributions = [i for i in range(1, 1001)]
 
     # random search
-    t = time.time()
-    distributions = [i for i in range(1, 1001)]
-    n_iter = 30
-    best_hidden_unit = randomSearch(distributions, x_train, y_train, x_test, y_test, n_iter)
+    start_time = time.time()
 
-    print("time for {} iteration random search: {}".format(n_iter, time.time() - t))
+    for i in range(no_of_exp):
+        print("{} experiment start ... ".format(i+1))
+        best_hidden_unit = randomSearch(distributions, x_train, y_train, x_test, y_test, no_of_trials, i)
+        model = create_model(best_hidden_unit)
+        model = train(model, x_train, y_train, x_test, y_test, 1)
+        accuracy = evaluate(model, x_test, y_test)
+        file_rs.write(str(best_hidden_unit) + "," + str(accuracy) + "\n")
+        print("{} experiment, best_hidden_unit: {}, accuracy: {}\n".format(i+1, best_hidden_unit, accuracy))
 
-    model = create_model(best_hidden_unit)
-    model = train(model, x_train, y_train, x_test, y_test, 2)
-    loss, accuracy = evaluate(model, x_test, y_test)
-    result = "{},{},{}".format(best_hidden_unit, loss, accuracy)
-    print("++ Result : best_hidden_unit, loss, accuracy \n \t\t" + result)
-    f = open("RS.csv", "w+")
-    f.write(result)
-    f.close()
+    elapsed_time = time.time() - start_time
+
+    print("time for {} experiment(s) random search: {} (second)".format(no_of_exp, elapsed_time))
+    file_rs.close()
 
 
     # model = KerasClassifier(build_fn=create_model)
